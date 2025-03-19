152b668878bc7c0636948969e068b0840b6a1990
Drawiel <daur0704@outlook.com>
2025-03-18 18:33:28 -0600

    Add project files.

---
+++ AromaCafeCliente/Windows/GUI_ProductDetails.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AromaCafeCliente.Windows {
+    /// <summary>
+    /// Interaction logic for GUI_ProductDetails.xaml
+    /// </summary>
+    public partial class GUI_ProductDetails : Page {
+        public GUI_ProductDetails() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AromaCafeCliente/Windows/UserControllers/GUI_PopUp_Stock.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AromaCafeCliente.Windows.UserControllers {
+    /// <summary>
+    /// Interaction logic for GUI_PopUp_Stock.xaml
+    /// </summary>
+    public partial class GUI_PopUp_Stock : UserControl {
+        public GUI_PopUp_Stock() {
+            InitializeComponent();
+        }
+    }
+}
