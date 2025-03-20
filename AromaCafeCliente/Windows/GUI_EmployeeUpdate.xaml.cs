aa5bd702d4a8430addbad15384e14b9436fab7e0
Drawiel <daur0704@outlook.com>
2025-03-20 03:16:32 -0600

    Se agrega funcionalida casi completa de 2 casos de uso

--- AromaCafeCliente/Windows/GUI_EmployeeUpdate.xaml.cs
+++ AromaCafeCliente/Windows/GUI_EmployeeUpdate.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,8 +19,99 @@ namespace AromaCafeCliente.Windows {
     /// Interaction logic for GUI_EmployeeUpdate.xaml
     /// </summary>
     public partial class GUI_EmployeeUpdate : Window {
+
+        private string employeType = " ";
+
         public GUI_EmployeeUpdate() {
             InitializeComponent();
+            radioButtonCashier.Checked += RadioButtonCheckedChanged;
+            radioButtonWaitress.Checked += RadioButtonCheckedChanged;
+            radioButtonManager.Checked += RadioButtonCheckedChanged;
         }
+
+        private void RadioButtonCheckedChanged(object sender, EventArgs e) {
+            RadioButton radioButton = sender as RadioButton;
+            employeType = radioButton.Tag.ToString();
+        }
+
+        private bool CheckAllFields() {
+            TextBox[] textBoxes = {
+                TxtBoxName,
+                txtBoxLastName,
+                txtBoxEmail,
+                txtBoxUser,
+                txtBoxStreet,
+                txtBoxCity,
+                txtBoxCP,
+                txtBoxNumber,
+            };
+
+            return textBoxes.All(tb => !string.IsNullOrWhiteSpace(tb.Text));
+        }
+
+        private bool IsValidEmail() {
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s+$";
+            return Regex.IsMatch(emailPattern, txtBoxEmail.Text);
+        }
+
+        private void BtnSaveClick(object sender, RoutedEventArgs e) {
+
+            if (CheckAllFields()) {
+                MessageBox.Show("Se ha actualizado correctamente la informacion del empleado");
+            } else if (!CheckAllFields() || !IsValidEmail()){
+                MessageBox.Show("Se han encontrado campos vacios o invalidos, favor de revisar");
+            }
+
+            radioButtonCashier.IsEnabled = false;
+            radioButtonWaitress.IsEnabled = false;
+            radioButtonManager.IsEnabled = false;
+            btnSave.IsEnabled = false;
+        }
+
+        private void BtnEditClick(object sender, RoutedEventArgs e) {
+            TextBox[] textBoxes = {
+                TxtBoxName,
+                txtBoxLastName,
+                txtBoxEmail,
+                txtBoxUser,
+                txtBoxStreet,
+                txtBoxCity,
+                txtBoxCP,
+                txtBoxNumber,
+            };
+
+            textBoxes.All(tb => tb.IsEnabled=true);
+            radioButtonCashier.IsEnabled = true;
+            radioButtonWaitress.IsEnabled = true;
+            radioButtonManager.IsEnabled = true;
+            btnSave.IsEnabled = true;
+        }
+
+        /*private bool UpdateProfile(Employee employee) { 
+            try {
+                int profileUpdated = userManager.UpdateProfile(employee);
+                if(profileUpdates != -1){
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (Exception){ 
+                return false;
+            }
+        }
+
+        private Employee CreateEmployee() {
+            var updatedEmployee = new Employee {
+                Name = TxtBoxName.Text,
+                LastName = txtBoxLastName.Text,
+                Email = txtBoxEmail.Text,
+                Username = txtBoxUser.Text,
+                PostalCode = txtBoxCP.Text,
+                EmployeeAddress = txtBoxStreet.Text + ", " + txtBoxCity.Text,
+                EmployeeType = employeType
+            };
+        }*/
+
+        
     }
 }
--- AromaCafeCliente/Windows/GUI_Employees.xaml.cs
+++ AromaCafeCliente/Windows/GUI_Employees.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +19,51 @@ namespace AromaCafeCliente.Windows {
     /// <summary>
     /// Interaction logic for GUI_Employees.xaml
     /// </summary>
-    public partial class GUI_Employees : Window {
+    public partial class GUI_Employees : Page {
+        
+        //private ObservableCollection<Employee> employeesList;
+        private ICollectionView employeesView;
+        
         public GUI_Employees() {
             InitializeComponent();
+            LoadEmployees();
         }
+
+        private void LoadEmployees() {
+            try {
+                /*employeeManager = new EmployeeManagerClient();
+                Employee[] employees = employeeManager.GetAllEmployee();
+                
+                foreach (var employee in employees){
+                    dataGridUsers.Rows.Add(employee.LastName, employee.Name, employee.EmployeeType, employee.EmployeeType);
+                }
+
+                employeesView = CollectionViewSource.GetDefaultView(employeesList);
+                employeesView.Filter = EmployeeFilter;
+
+                dataGridUser.ItemsSource = employeesView;
+                 */
+            } catch (Exception){
+                MessageBox.Show("Ocurrio un error recuperando los empleados");
+            }
+        }
+
+        private void TxtSearchBoxTextChanged(object sender, TextChangedEventArgs e) {
+            if (employeesView != null) {
+                employeesView.Refresh(); 
+            }
+        }
+
+        /*private bool EmployeeFilter(object item) {
+            if (string.IsNullOrEmpty(txtSearchBox.Text)) return true;
+
+            var employee = item as Employee;
+            string searchText = txtSearchBox.Text.ToLower();
+
+            return employee.LastName.ToLower().Contains(searchText) ||
+                   employee.Name.ToLower().Contains(searchText) ||
+                   employee.Type.ToLower().Contains(searchText) ||
+                   employee.Status.ToLower().Contains(searchText);
+        }*/
     }
 }
