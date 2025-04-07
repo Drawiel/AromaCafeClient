ef232033b9e78fc6eb14c0c8907233df6ba59a92
Isabella Méndez <isabella.hmdez@gmail.com>
2025-04-07 16:23:15 -0600

    user controls de registrar pago y cerrar sesión Nuevas GUIS: GUI_CheckCashier GUI_Reports GUI_TablesCashier
    Cambios en:
    GUI_HomeManager
    GUI_HomeWaitress
    GUI_NewProduct
    GUI_ProductDetails

---
+++ AromaCafeCliente/Windows/GUI_Reports.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AromaCafeCliente.Windows {
+    /// <summary>
+    /// Lógica de interacción para GUI_Reports.xaml
+    /// </summary>
+    public partial class GUI_Reports : Page {
+        public GUI_Reports() {
+            InitializeComponent();
+        }
+    }
+}
